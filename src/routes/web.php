a487948ae86b697dc2a53ab53dc4fb04ebb4d42a
Aldo <aldopjetri42@gmail.com>
2024-01-11 14:44:54 +0100

    commit

--- src/routes/web.php
+++ src/routes/web.php
@@ -3,7 +3,7 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('verify-key', function () {
-    return view(base_path('vendor') . '\ap24\package_for_agencies\src/views/verify-secret-key');
+    return view(base_path('vendor') . '\ap24\package_for_agencies\src\views\verify-secret-key');
 })->name('verify-key');
 
 Route::post('verify-secret-key', [\Ap24\PackageForAgencies\Controllers\ActivationController::class, 'activateKey'])->name('activate-key');
