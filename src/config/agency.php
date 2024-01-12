b01293dbb990bc2cf5b47be4efa8bba190a66501
Aldo <aldopjetri42@gmail.com>
2024-01-12 15:43:11 +0100

    final

--- src/config/agency.php
+++ src/config/agency.php
@@ -3,5 +3,4 @@
 return [
     'verify_key' => 'http://192.168.2.13:8000/api/secret-keys/verify-secret-key',
     'check_key_activation' => 'http://192.168.2.13:8000/api/secret-keys/check-key-activation',
-    'redirect' => '/',
 ];
